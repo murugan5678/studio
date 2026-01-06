f494bbbac17d96f823422a549f60dd1b6592454b
Murugan R <murugan@qaoncloud.com>
2026-01-06 17:46:24 +0000

    Try fixing this error: `Console Error: Body exceeded 1 MB limit. To conf

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverActions: {
+    bodySizeLimit: '5mb',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
