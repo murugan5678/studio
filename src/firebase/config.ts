ee1155fc9ece8e780228399106e2a1be7c328cc7
Murugan R <murugan@qaoncloud.com>
2026-01-06 07:16:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2485256183-6b7b0",
+  "appId": "1:13454296466:web:4c1231b891f4874d54ed4b",
+  "apiKey": "AIzaSyBrgdXBDS_zSYklKzyaU9RTpwSv1fNtsUc",
+  "authDomain": "studio-2485256183-6b7b0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "13454296466"
+};
