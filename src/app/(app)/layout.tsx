cb2da5f974a2bf37307ba592a4d5c4e684033539
Murugan R <murugan@qaoncloud.com>
2026-01-06 14:55:39 +0000

    add the app backgriund collour which is professional

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,7 +5,7 @@ import { Sidebar } from '@/components/layout/sidebar';
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthGuard>
-      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="flex min-h-screen w-full flex-col bg-background">
         <Sidebar />
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-60">
             <Header />
